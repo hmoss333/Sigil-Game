abf4754beb738eefbead6477c7188631acc23b3d
hmoss333 <hdamoss@gmail.com>
2018-01-29 14:07:32 -0500

    Update 1/29/2018 pt.2
    -Added trigger boxes

---
+++ Assets/Scripts/TriggerTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTest : MonoBehaviour {
+
+    Renderer renderer;
+    
+
+    // Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        renderer.material.color = Color.white;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Line")
+        {
+            renderer.material.color = Color.red;
+            Debug.Log("tiggering");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Line")
+        {
+            renderer.material.color = Color.white;
+            Debug.Log("not triggering");
+        }
+    }
+}
