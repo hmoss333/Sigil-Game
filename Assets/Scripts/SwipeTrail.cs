31bc5e51f08035fe4df4302f4b824fd972f7aaf5
hmoss333 <hdamoss@gmail.com>
2018-01-29 13:47:10 -0500

    Update 1/29/2018
    -Updated code to allow for multiple inputs at a time
    -Increased line lifetime to 1s

--- Assets/Scripts/SwipeTrail.cs
+++ Assets/Scripts/SwipeTrail.cs
@@ -4,22 +4,46 @@ using UnityEngine;
 
 public class SwipeTrail : MonoBehaviour {
 
-	// Use this for initialization
+    //Built using these guides:
+    //https://www.youtube.com/watch?v=cHVZ0SYIHkI
+    //https://www.youtube.com/watch?v=xlwuGKTyJBs&feature=youtu.be
+
+    public GameObject trailPrefab;
+    GameObject thisTrail;
+    Vector3 startPos;
+    Plane objPlane;
+    
+    // Use this for initialization
 	void Start () {
-		
+        objPlane = new Plane(Camera.main.transform.forward *= 1, this.transform.position);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
-            || Input.GetMouseButton(0)))
+		if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) ||
+            Input.GetMouseButtonDown(0)))
         {
-            Plane objPlane = new Plane(Camera.main.transform.forward *= 1, this.transform.position);
-
+            thisTrail = (GameObject)Instantiate(trailPrefab, 
+                                                    this.transform.position, 
+                                                    Quaternion.identity);
             Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             float rayDistance;
             if (objPlane.Raycast(mRay, out rayDistance))
-                this.transform.position = mRay.GetPoint(rayDistance);
+                startPos = mRay.GetPoint(rayDistance);
+        }
+        else if (((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || 
+            Input.GetMouseButton(0)))
+        {
+            Ray mRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float rayDistance;
+            if (objPlane.Raycast(mRay, out rayDistance))
+                thisTrail.transform.position = mRay.GetPoint(rayDistance);
+        }
+        else if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || 
+            Input.GetMouseButtonUp(0))
+        {
+            if (Vector3.Distance(thisTrail.transform.position, startPos) < 0.1f)
+                Destroy(thisTrail);
         }
 	}
 }
