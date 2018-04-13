a5fe4d21c9d48c6d82a137fe46da3669a551c562
hmoss333 <hdamoss@gmail.com>
2018-04-13 14:38:50 -0400

    Update 4/13/2018
    -Started adding framework for saving to image gallery on Android

--- Assets/Scripts/Gameplay/HiResScreenShots.cs
+++ Assets/Scripts/Gameplay/HiResScreenShots.cs
@@ -45,6 +45,44 @@ public class HiResScreenShots : MonoBehaviour {
         takeHiResShot = true;
     }
 
+    private const string MediaStoreImagesMediaClass = "android.provider.MediaStore$Images$Media";
+    private static AndroidJavaObject _activity;
+
+    public static AndroidJavaObject Activity
+        {
+            get
+            {
+                if (_activity == null)
+                {
+                    var unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+                    _activity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+                }
+                return _activity;
+            }
+        }
+
+    public static string SaveImageToGallery(Texture2D texture2D, string title, string description)
+    {
+        using (var mediaClass = new AndroidJavaClass(MediaStoreImagesMediaClass))
+        {
+            using (var cr = Activity.Call<AndroidJavaObject>("getContentResolver"))
+            {
+                var image = Texture2DToAndroidBitmap(texture2D);
+                var imageUrl = mediaClass.CallStatic<string>("insertImage", cr, image, title, description);
+                return imageUrl;
+            }
+        }
+    }
+
+    public static AndroidJavaObject Texture2DToAndroidBitmap(Texture2D texture2D)
+    {
+        byte[] encoded = texture2D.EncodeToPNG();
+        using (var bf = new AndroidJavaClass("android.graphics.BitmapFactory"))
+        {
+            return bf.CallStatic<AndroidJavaObject>("decodeByteArray", encoded, 0, encoded.Length);
+        }
+    }
+
     void LateUpdate()
     {
         takeHiResShot |= Input.GetKeyDown("k");
@@ -63,6 +101,9 @@ public class HiResScreenShots : MonoBehaviour {
             byte[] bytes = screenShot.EncodeToPNG();
             string filename = ScreenShotName(SigilManager.screenShotName);
             System.IO.File.WriteAllBytes(filename, bytes);
+//#if UNITY_ANDROID
+            //SaveImageToGallery(screenShot, SigilManager.screenShotName, SigilManager.screenShotName);
+//#endif
             Debug.Log(string.Format("Took screenshot to: {0}", filename));
             camera.cullingMask |= 1 << LayerMask.NameToLayer("UI"); //Show UI layer
             takeHiResShot = false;
